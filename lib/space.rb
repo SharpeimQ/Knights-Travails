65a63f704fffb221d0436d09fcafdc64818668a5
SharpeimQ <SharpHaiyan@gmail.com>
2023-05-16 20:20:41 -0400

    Build algorithm to find all legal knight moves

--- lib/space.rb
+++ lib/space.rb
@@ -4,8 +4,28 @@
 class Space
   attr_accessor :position, :moves
 
-  def initialize(position = nil, moves = [])
+  def initialize(position = nil)
     @position = position
-    @moves = moves
+    @moves = move_calc(position)
+  end
+
+  def move_calc(position)
+    move_offsets = {
+      [-2, 1] => true, [-1, 2] => true, [1, 2] => true, [2, 1] => true,
+      [2, -1] => true, [1, -2] => true, [-1, -2] => true, [-2, -1] => true
+    }
+    move_calc_h(move_offsets, position)
+  end
+
+  def move_calc_h(hash, position)
+    moves = []
+
+    hash.each_key do |key|
+      x = key[0] + position[0]
+      y = key[1] + position[1]
+
+      moves << [x, y] if x.between?(0, 7) && y.between?(0, 7)
+    end
+    moves
   end
 end
